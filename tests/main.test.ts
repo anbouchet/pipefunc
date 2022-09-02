b575eb2155f723b67e35de771b3b53b651ead7c5
Ancelin Bouchet <ancelin@bouchet.info>
2022-09-02 14:30:52 +0200

    test: added reversePipeline tests

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -72,6 +72,66 @@ describe('pipefunc tests', () => {
     });
   });
 
+  describe('`reversePipeline` function tests', () => {
+    test('Reverse pipeline function with one function', () => {
+      // Given
+      const parseNum = getMocks()[0];
+
+      // When
+      const builder = reversePipeline(parseNum);
+
+      // Then
+      expect(builder).toBeInstanceOf(Function);
+      expect(parseNum).toHaveBeenCalledTimes(0);
+    });
+
+    test('Building reverse pipeline with one function', () => {
+      // Given
+      const parseNum = getMocks()[0];
+
+      // When
+      const func = reversePipeline(parseNum)();
+
+      // Then
+      expect(typeof func).toBe('function');
+      expect(parseNum).toHaveBeenCalledTimes(0);
+    });
+
+    test('Built reverse pipeline with one function processes input when called', () => {
+      // Given
+      const parseNum = getMocks()[0];
+      const value = someString();
+
+      // When
+      const func = reversePipeline(parseNum)();
+      const result = func(value);
+
+      // Then
+      expect(parseNum).toHaveBeenCalledTimes(1);
+      expect(parseNum).toHaveBeenCalledWith(value);
+      expect(result).toBe(someNumber());
+    });
+
+    test('Building pipeline with multiple functions', () => {
+      // Given
+      const [parseNum, timesFive] = getMocks();
+      const value = someString();
+
+      // When
+      const func = reversePipeline(timesFive)(parseNum)();
+      const result = func(value);
+
+      // Then
+      expect(result).toBe(someNumberTimesFive());
+      expect(parseNum).toHaveBeenCalledTimes(1);
+      expect(parseNum).toHaveBeenCalledWith(value);
+      expect(timesFive).toHaveBeenCalledTimes(1);
+      expect(timesFive).toHaveBeenCalledWith(someNumber());
+      const firstCallOrder = parseNum.mock.invocationCallOrder[0];
+      expect(timesFive.mock.invocationCallOrder[0]).toBe(firstCallOrder + 1);
+    });
+  });
+
   describe('`pipe` function tests', () => {
     test('Pipe function call returns a function', () => {
       //Given
