2439f4d4926219f8072659ae3600c92c9aac9df5
Ancelin Bouchet <ancelin@bouchet.info>
2022-09-02 14:12:20 +0200

    test: Added pipeline tests

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -12,6 +12,66 @@ function getMocks(): [(x: string) => number, (x: number) => number] {
 const someString = () => '42';
 
 describe('pipefunc tests', () => {
+  describe('`pipeline` function tests', () => {
+    test('Pipeline function with one function', () => {
+      // Given
+      const parseNum = getMocks()[0];
+      const value = someString();
+
+      // When
+      const builder = pipeline(parseNum);
+
+      // Then
+      expect(builder).toBeInstanceOf(Function);
+      expect(parseNum).toHaveBeenCalledTimes(0);
+    });
+
+    test('Building pipeline with one function', () => {
+      // Given
+      const parseNum = getMocks()[0];
+      const value = someString();
+
+      // When
+      const func = pipeline(parseNum)();
+
+      // Then
+      expect(typeof func).toBe('function');
+      expect(parseNum).toHaveBeenCalledTimes(0);
+    });
+
+    test('Built pipeline with one function processes input when called', () => {
+      // Given
+      const parseNum = getMocks()[0];
+      const value = someString();
+
+      // When
+      const func = pipeline(parseNum)();
+      const result = func(value);
+
+      // Then
+      expect(parseNum).toHaveBeenCalledTimes(1);
+      expect(parseNum).toHaveBeenCalledWith(value);
+      expect(result).toBe(someNumber());
+    });
+
+    test('Building pipeline with multiple functions', () => {
+      // Given
+      const [parseNum, timesFive] = getMocks();
+      const value = someString();
+
+      // When
+      const func = pipeline(parseNum)(timesFive)();
+      const result = func(value);
+
+      // Then
+      expect(result).toBe(someNumberTimesFive());
+      expect(parseNum).toHaveBeenCalledTimes(1);
+      expect(parseNum).toHaveBeenCalledWith(value);
+      expect(timesFive).toHaveBeenCalledTimes(1);
+      expect(timesFive).toHaveBeenCalledWith(someNumber());
+    });
+  });
+
   describe('`pipe` function tests', () => {
     test('Pipe function call returns a function', () => {
       //Given
