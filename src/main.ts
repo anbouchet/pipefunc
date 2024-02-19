76237ef750443a4f9fe64774db50b407b9e1678e
Ancelin Bouchet <ancelin@bouchet.info>
2024-02-19 19:20:35 +0100

    refactor: Made pipeline and reversePipeline internals use array reduction instead of recursion when executed

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ type UnaryOperator<I extends [x: unknown] = [x: unknown], R = unknown> = (...x: 
 /** A function that can take multiple arguments */
 type MultiFunction<I extends [...x: unknown[]], R = unknown> = (...x: I) => R;
 
+/** A type to wrap any function, for internal use only after adequate type checks */
+type AnyFunction = (...a: unknown[]) => unknown;
+
 /**
  * Builds the pipeline by allowing to add functions to be executed after others in the pipeline
  */
@@ -27,16 +30,38 @@ export interface PipelineBuilder<P extends unknown[], R> {
  * // value in t is a function that executes fun1, then fun2 and returns it's return value
  * const t = pipeline(fun1)(fun2)()
  * ```
- * @param a The first function in the pipeline
+ * @param func The first function in the pipeline
  * @returns A new pipeline builder
  */
-export function pipeline<P extends unknown[], R>(a: MultiFunction<P, R>): PipelineBuilder<P, R> {
-  return function <U>(b?: UnaryOperator<[R], U>) {
-    if (b == null) {
-      return a;
+export function pipeline<P extends unknown[], R>(func: MultiFunction<P, R>): PipelineBuilder<P, R> {
+  return pipelineBuilder<P, R>(func as AnyFunction);
+}
+
+/**
+ * Create an executor function, which when called with the right arguments, calls each function of the pipe in order, piping through the results
+ * @param pipeline The array of functions to execute in order
+ * @returns A function that executes and pipes through the results
+ */
+const makeExec =
+  <P extends unknown[], R>(pipeline: AnyFunction[]) =>
+  (...args: P) =>
+    pipeline.reduce((args, func) => [func(...args)], args as unknown[])[0] as R;
+
+/**
+ * Creates the builder function, which memorizes the current pipeline.
+ * @param func The next function to add to the pipeline array
+ * @param previous The previous pipeline array, defaults to an empty array
+ * @returns A function which decides whether to continue to create a pipeline or return an execution function based on its arguments
+ */
+function pipelineBuilder<P extends unknown[], R>(func: AnyFunction, previous: AnyFunction[] = []): PipelineBuilder<P, R> {
+  const pipeline = [...previous, func];
+
+  return function <U>(func?: UnaryOperator<[R], U>) {
+    if (func != null) {
+      return pipelineBuilder<P, U>(func as AnyFunction, pipeline);
     }
 
-    return pipeline((...x: P) => b(a(...x)));
+    return makeExec<P, R>(pipeline);
   } as PipelineBuilder<P, R>;
 }
 
@@ -82,23 +107,25 @@ export interface ReversePipelineBuilder<P extends unknown[], R> {
  * // value in t is a function that executes fun2, then fun1 and returns it's return value
  * const t = reversePipeline(fun1)(fun2)()
  * ```
- * @param a Last function to be executed in the pipeline
+ * @param func Last function to be executed in the pipeline
  * @returns A new pipeline builder function
  */
-export function reversePipeline<P extends [unknown], R>(a: UnaryOperator<P, R>): ReversePipelineBuilder<P, R> {
-  return function <U extends unknown[]>(b?: MultiFunction<U, P[0]>) {
-    if (b == null) {
-      return a;
+export function reversePipeline<P extends [unknown], R>(func: UnaryOperator<P, R>): ReversePipelineBuilder<P, R> {
+  return reversePipelineBuilder<P, R>(func as AnyFunction);
+}
+
+function reversePipelineBuilder<P extends unknown[], R>(func: AnyFunction, next: AnyFunction[] = []): ReversePipelineBuilder<P, R> {
+  const pipeline = [func, ...next];
+
+  return function <U extends unknown[]>(func?: MultiFunction<U, P[0]>) {
+    if (func != null) {
+      if (func.length > 1) {
+        return () => makeExec<P, R>(pipeline);
+      }
+      return reversePipelineBuilder<P, U>(func as AnyFunction, pipeline);
     }
 
-    if (b.length > 1) {
-      return <V extends U>() =>
-        (...x: V) =>
-          // @ts-ignore return value of b is checked to be compatible with param type of a
-          a(b(...x));
-    }
-    // @ts-ignore return value of b is checked to be compatible with param type of a
-    return reversePipeline(((...x: U) => a(b(...x))) as unknown as UnaryOperator<[U[0]], R>);
+    return makeExec<P, R>(pipeline);
   } as ReversePipelineBuilder<P, R>;
 }
 
